b673a161cc4121ac049905488148e6b26f35c75c
CheoR <cheo.roman+github@gmail.com>
2021-05-10 07:11:27 -0500

    doc: add event view set.

--- levelupapi/views/__init__.py
+++ levelupapi/views/__init__.py
@@ -1,7 +1,12 @@
 """Views Module"""
 from .auth import login_user
 from .auth import register_user
+
 from .gameviewset import GameViewSet
+from .gameserializer import GameSerializer
+
 from .gametypeviewset import GameTypeViewSet
 from .gametypeserializer import GameTypeSerializer
-from .gameserializer import GameSerializer
+
+from .eventviewset import EventViewSet
+from .eventserializer import EventSerializer
