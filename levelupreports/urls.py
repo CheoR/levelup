3d73fce607b32b8036cecdb489545079432623ea
CheoR <cheo.roman+github@gmail.com>
2021-05-27 15:02:44 -0500

    doc: add url endpoints for the app to the python functions that will address client request and create a template. Then add the app urls to the project urls.

---
+++ levelupreports/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import usergame_list
+
+urlpatterns = [
+    # where the first slot is the endpoint
+    # and the second is the function that will handle the
+    # request when client hits that endpoint
+    path('reports/usergames', usergame_list),
+]
